fc92d7da67c4cc61d7a45e469b6ff8cb4c358236
Khodor Hassoun <khodorhassoun@outlook.com>
2023-03-10 14:22:41 +0200

    add nodes and print nodes

--- index.js
+++ index.js
@@ -1,30 +1,58 @@
-const hashObj = {
-  0: 0,
-  1: 0,
-  2: 0,
-  3: 0,
-  4: 0,
-  5: 0,
-};
-
-const countArray = [];
-const sortedArray = [];
-
-// Array determined has elements exclusively between 0 and 5
-
-function countAndSort(array) {
-  for (elem of array) {
-    hashObj[elem] += 1;
-  }
-  Object.values(hashObj).forEach((val) => countArray.push(val));
-  //   return countArray;
-
-  for (let i = 0; i < countArray.length; i++) {
-    let k = 0;
-    while (k < countArray[i]) {
-      sortedArray.push(i);
-      k++;
+var PrivateNode = /** @class */ (function () {
+    function PrivateNode(value) {
+        this.val = value;
     }
-  }
-  return { countArray, sortedArray };
-}
+    return PrivateNode;
+}());
+var LinkedList = /** @class */ (function () {
+    function LinkedList(value) {
+        this.head = new PrivateNode(value);
+        this.size = 1;
+        this.head.next = null;
+    }
+    LinkedList.prototype.addNode = function (value) {
+        // if size 1 then only add to next of head
+        var newNode = new PrivateNode(value);
+        if (this.size === 1) {
+            this.head.next = newNode;
+            this.size++;
+            newNode.next = null;
+        }
+        else {
+            // we will have to loop through the list
+            var iteratorNode = this.head;
+            while (iteratorNode.next !== null) {
+                iteratorNode = iteratorNode.next;
+            }
+            iteratorNode.next = newNode;
+            this.size++;
+        }
+    };
+    LinkedList.prototype.print = function () {
+        if (this.size === 1) {
+            return [this.head.val];
+        }
+        var iteratorNode = this.head;
+        var container = [];
+        while (iteratorNode.next !== null) {
+            container.push(iteratorNode.val);
+            iteratorNode = iteratorNode.next;
+        }
+        return container;
+    };
+    LinkedList.prototype.removeNode = function (value) {
+        // If first node remove first
+        var iteratorNode = this.head;
+        while (iteratorNode.next !== null) {
+            if (iteratorNode.val > value) {
+            }
+        }
+    };
+    LinkedList.prototype.removeHeadNode = function () {
+        if (this.head.next) {
+            this.head = this.head.next;
+        }
+    };
+    return LinkedList;
+}());
+// export {};
---
+++ index.ts
@@ -0,0 +1,63 @@
+class PrivateNode {
+  val: number;
+  next: PrivateNode | null;
+
+  constructor(value: number) {
+    this.val = value;
+  }
+}
+class LinkedList {
+  head: PrivateNode;
+  size: number;
+
+  constructor(value: number) {
+    this.head = new PrivateNode(value);
+    this.size = 1;
+    this.head.next = null;
+  }
+
+  addNode(value: number): void {
+    // if size 1 then only add to next of head
+    const newNode = new PrivateNode(value);
+    if (this.size === 1) {
+      this.head.next = newNode;
+      this.size++;
+      newNode.next = null;
+    } else {
+      // we will have to loop through the list
+      let iteratorNode = this.head;
+      while (iteratorNode.next !== null) {
+        iteratorNode = iteratorNode.next;
+      }
+      iteratorNode.next = newNode;
+      this.size++;
+    }
+  }
+  print(): number[] {
+    if (this.size === 1) {
+      return [this.head.val];
+    }
+    let iteratorNode = this.head;
+    const container: number[] = [];
+    while (iteratorNode.next !== null) {
+      container.push(iteratorNode.val);
+      iteratorNode = iteratorNode.next;
+    }
+    return container;
+  }
+
+  removeNode(value: number): void {
+    // If first node remove first
+    let iteratorNode = this.head;
+    while (iteratorNode.next !== null) {
+      if (iteratorNode.val > value) {
+      }
+    }
+  }
+  removeHeadNode(): void {
+    if (this.head.next) {
+      this.head = this.head.next;
+    }
+  }
+}
+// export {};
