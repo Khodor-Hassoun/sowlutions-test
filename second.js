177bf8898a9b8e4073d770152f36db4482a7b285
Khodor Hassoun <khodorhassoun@outlook.com>
2023-03-10 20:55:19 +0200

    Add documentation

--- second.js
+++ second.js
@@ -26,29 +26,29 @@ function isValidBracketSequence(str) {
   }
 
   while (i < cleanArray.length - 1) {
+    // get the index of the opening bracket in opening array
     let openingIndex = opening.indexOf(cleanArray[i]);
+
+    // I only worry about opening and if they have a closing or not, it is safe to say that a closing bracket is handled by an opening bracket case
     if (openingIndex === -1) {
       i++;
       continue;
     }
+
+    // i is the element to be tested in given array, j is how many characters far from the closing bracket is
     if (cleanArray[i + j + 1] !== closing[openingIndex]) {
       if (opening.indexOf(cleanArray[i + j]) === -1) {
         return `FIRST FALSE i = ${i}  j = ${j} opening index = ${openingIndex}`;
       } else {
+        // if the opening bracket direct neighbor is not its closing, but is also an opening then add j by 2 (the opening and closing)
         j += 2;
       }
     }
-    // if (i + j === cleanArray.length - 1) {
-    //   i++;
-    // } else if (cleanArray[i + j] === closing[openingIndex]) {
-    // } else {
-    //   return false;
-    // }
+
+    // the bracket has closing go to next
     if (cleanArray[i + j + 1] === closing[openingIndex]) {
       i++;
       j = 0;
-      // } else if (j !== cleanArray.length - i) {
-      //   j++;
     } else if (j > cleanArray.length) {
       return "SECOND FALSE";
     }
